4e9b8122b2e2061a323ec345fd7d44e9adcb2263
Tiago Silveira <tiagojesusilveira@gmail.com>
2026-01-16 13:03:56 +0100

    Update with ex02

---
+++ ex02/includes/MutantStack.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilveir <tsilveir@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 17:29:01 by tsilveir          #+#    #+#             */
+/*   Updated: 2026/01/16 13:01:30 by tsilveir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <exception>
+#include <stack>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+template<typename T>
+class MutantStack: public std::stack<T> {
+public:
+	MutantStack(): std::stack<T>() {};
+	MutantStack(MutantStack& other): std::stack<T>(other) {};
+	MutantStack &operator=(MutantStack& other){
+		if (this != &other )
+			std::stack<T>::operator=(other);
+		return *this;
+	};
+	~MutantStack() {};
+
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  iterator begin() {return this->c.begin();};
+  iterator end() {return this->c.end();};
+
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+  const_iterator begin() const {return this->c.begin();};
+  const_iterator end() const {return this->c.end();};
+
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+  reverse_iterator rbegin() {return this->c.rbegin();};
+  reverse_iterator rend() {return this->c.rend();};
+
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+	const_reverse_iterator rbegin() const {return this->c.rbegin();}; 
+	const_reverse_iterator rend() const {return this->c.rend();}; 
+};
---
+++ ex02/src/main.cpp
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilveir <tsilveir@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 17:29:14 by tsilveir          #+#    #+#             */
+/*   Updated: 2026/01/16 13:02:22 by tsilveir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/MutantStack.hpp"
+#include <list>
+#include <vector>
+
+#define SIZE_VEC 10
+
+int main()
+{
+	srand(time(NULL));
+
+	std::vector<int>	array_num;
+	for (int i = 0; i < SIZE_VEC; i++)
+	{
+		array_num.push_back(rand() % 500);
+	}
+
+	std::cout << "[Mutant Stack Section]\n\n"; 
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	for (int i = 0; i < SIZE_VEC; i++)
+	{
+		mstack.push(array_num.at(i));
+	}
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "[List Section]\n\n"; 
+
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << lst.back() << std::endl;
+	lst.pop_back();
+	std::cout << lst.size() << std::endl;
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	for (int i = 0; i < SIZE_VEC; i++)
+	{
+		lst.push_back(array_num.at(i));
+	}
+	lst.push_back(0);
+	std::list<int>::iterator lst_it = lst.begin();
+	std::list<int>::iterator lst_ite = lst.end();
+	++lst_it;
+	--lst_it;
+	while (lst_it != lst_ite)
+	{
+		std::cout << *lst_it << std::endl;
+		++lst_it;
+	}
+	std::stack<int, std::list<int> > s_lst(lst);
+	return 0;
+}
